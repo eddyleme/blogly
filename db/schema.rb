832a6fe507e62e44b5b9d775a30441e474136d98
Eduardo Leme <eddyleme@gmail.com>
2018-04-04 18:27:06 -0400

    add admin model with email and password

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401041340) do
+ActiveRecord::Schema.define(version: 20180404222621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
